c40a1ccb1e6afb813d85c44ea4314884ac654b4f
jmbada <jmbada@gmail.com>
2017-02-05 15:28:34 -0800

    Add files via upload

---
+++ character_input.py
@@ -0,0 +1,19 @@
+# Getting user input
+# Manipulating strings (a few ways)
+
+name = raw_input('Give me your name: ')
+print 'Your name is %s ' %name
+
+age = input("Enter your age: ")
+age = int(age)
+
+#age = int(input("Enter your age: ")
+
+print "Were" + "wolf"
+print "Door" + "man"
+print "4" + "chan"
+print str(4) + "Chan"
+
+print 4 * "test"
+
+
